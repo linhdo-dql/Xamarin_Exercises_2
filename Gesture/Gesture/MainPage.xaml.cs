adcbca8cf845f2b895f05485e8f2037d87f2f882
linhdo <linhlee88999@gmai.com>
2021-12-21 16:55:25 +0700

    Gesture Demo

---
+++ Gesture/Gesture/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Gesture
+{
+    public partial class MainPage : ContentPage
+    {
+        int oneTapCount = 0;
+        int doubleTapCount = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OneTap_Gesture(Object sender, EventArgs e)
+        {
+            oneTapCount++;
+            lblOneTap.Text = "Tap: "+ oneTapCount;
+        }
+
+        private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+        {
+            var label = (sender as Element)?.Parent as Label;
+            e.Data.Properties.Add("Text", label.Text);
+        }
+
+        private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+        {
+            var data = e.Data.Properties["Text"];
+            var frame = (sender as Element)?.Parent as Frame;
+            frame.Content = new Label()
+            {
+                Text = data.ToString()
+
+            };
+            
+
+        }
+    }
+}
