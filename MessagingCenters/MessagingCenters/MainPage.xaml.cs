a09619b51fc20a011897dbcd4861df4b7d356deb
linhdo <linhlee88999@gmai.com>
2021-12-21 16:43:28 +0700

    MessagingCenter Demo

---
+++ MessagingCenters/MessagingCenters/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessagingCenters
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public ObservableCollection<string> Messages { get; set; } = new ObservableCollection<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void btnSendMessage_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<Page, DateTime>(this, "tick", (p, datetime) =>
+            {
+                 Messages.Add("Sucrices at "+datetime.ToString("dd/MM/yyyy HH:mm:ss f"));
+            });
+        }
+        private async void btnGoToPage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new ReceivePage());
+        }
+    }
+}
---
+++ MessagingCenters/MessagingCenters/ReceivePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessagingCenters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReceivePage : ContentPage
+    {
+
+        public ReceivePage()
+        {
+            InitializeComponent();
+        }
+        private void btnSendMessage_Clicked(Object sender, EventArgs e)
+        {
+            MessagingCenter.Send<Page, DateTime>(this, "tick", DateTime.Now);
+        }
+        private void btnBackPage_Clicked(Object sender, EventArgs e)
+        {
+            Navigation.PopModalAsync();
+        }
+    }
+}
